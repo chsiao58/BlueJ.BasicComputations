4b68c3f329e003ccd35081435634fd40f434ded7
Joanna Hsiao <chsiao58@gmail.com>
2019-05-23 16:27:11 -0400

    all test passed

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,14 @@ import java.math.BigInteger;
 public class Factorial {
 
     public BigInteger factorialOf(Integer value){
-        return null;
+        BigInteger factorial = BigInteger.ONE;
+        BigInteger current = BigInteger.ONE;
+        for (Integer i = 1; i <= value; i++)
+        {
+            factorial = factorial.multiply(current);
+            current = current.add(BigInteger.ONE);
+        }
+        return factorial;
     }
 
 }
--- LargestInteger.java
+++ LargestInteger.java
@@ -4,10 +4,15 @@
 public class LargestInteger {
 
     public Integer findLargestNumberUsingConditional(Integer[] integers){
-        return null;
+        Integer Largest = integers[0];
+        for(int i = 1; i < integers.length; i++)
+            if (integers[i] > Largest)
+                Largest = integers[i];
+        return Largest;
     }
 
     public Integer findLargestNumberUsingMathMax(Integer[] integers){
-        return null;
+        
+        return Math.max(integers[0],Math.max(integers[1],integers[2]));
     }
 }

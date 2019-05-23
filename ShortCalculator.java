cfbe732c680325038d6fea8a80962dd607a6aa4f
Joanna Hsiao <chsiao58@gmail.com>
2019-05-23 16:28:47 -0400

    Not sure about the part about not coverting to int

--- ShortCalculator.java
+++ ShortCalculator.java
@@ -1,5 +1,31 @@
  
 
 
-public class ShortCalculator {
+public class ShortCalculator 
+{
+    private short s1;
+    private short s2;
+    public void setTwoShort (short newS1, short newS2)
+    {
+        s1 = newS1;
+        s2 = newS2;
+    }
+    
+    public short[] getTwoShort()
+    {
+        short[] twoShort = {s1,s2};
+        return twoShort;
+    }
+    
+    // Is it even possible to do short addition without converting to int?
+    // addition operator like below will automaticaliy convert short to int
+    public short unsignedSum()
+    {
+        return (short) (s1+s2);
+    }
+    
+    public short unsignedDiff()
+    {
+        return (short) (s1-s2);
+    }
 }

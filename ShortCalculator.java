a54ca5aa6d8c4327c266b9b59f6ab2ec667050d4
Joanna Hsiao <chsiao58@gmail.com>
2019-05-23 17:39:47 -0400

    My attempt at short calculator

--- ShortCalculator.java
+++ ShortCalculator.java
@@ -1,31 +1,48 @@
- 
-
-
 public class ShortCalculator 
 {
     private short s1;
     private short s2;
-    public void setTwoShort (short newS1, short newS2)
+    private final int VAL_TO_UNSIGNED_SHORT = 32768;
+    public void setTwoShort (int newS1, int newS2)
     {
-        s1 = newS1;
-        s2 = newS2;
+        if(newS1 >= 0 && newS1 <= 65535 && newS2 >= 0 && newS2 <= 65535 )
+        {
+            s1 = (short)(newS1-VAL_TO_UNSIGNED_SHORT);
+            s2 = (short)(newS2-VAL_TO_UNSIGNED_SHORT);
+        }
     }
     
-    public short[] getTwoShort()
+    public int[] getTwoShort()
     {
-        short[] twoShort = {s1,s2};
+        int[] twoShort = {s1+VAL_TO_UNSIGNED_SHORT,s2+VAL_TO_UNSIGNED_SHORT};
         return twoShort;
     }
     
-    // Is it even possible to do short addition without converting to int?
+    // Is it even possible to do without converting to int?
     // addition operator like below will automaticaliy convert short to int
     public short unsignedSum()
     {
-        return (short) (s1+s2);
+        return (short)(s1+s2);
     }
     
     public short unsignedDiff()
     {
-        return (short) (s1-s2);
+        return (short)(s1-s2);
+    }
+    
+    public short unsignedProduct()
+    {
+        return (short)(s1*s2+VAL_TO_UNSIGNED_SHORT);
+    }
+    
+    public short unsignedQuotient()
+    {
+        return (short)Math.abs(
+        s1+VAL_TO_UNSIGNED_SHORT/s2+VAL_TO_UNSIGNED_SHORT);
+    }
+    
+       public short unsignedRemainder()
+    {
+        return (short)(s1+VAL_TO_UNSIGNED_SHORT%s2+VAL_TO_UNSIGNED_SHORT);
     }
 }
